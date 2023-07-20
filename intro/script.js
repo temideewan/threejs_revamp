17cdfd6df1fb0e04f008d7d66ba667118821993c
Omoyajowo Temidayo <temideewan@gmail.com>
2023-07-20 20:11:23 +0100

    feat: first scene set up and render a cube 🔥💃🏻

---
+++ intro/script.js
@@ -0,0 +1,31 @@
+// scene
+const scene = new THREE.Scene();
+
+// Red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000})
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+ 
+// sizes
+const sizes = {
+  width: 800,
+  height: 600
+}
+// camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height)
+camera.position.z = 3
+camera.position.x = -1
+camera.position.y = 1
+scene.add(camera);
+
+const canvas = document.querySelector(".webgl")
+console.log(canvas);
+// renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera)
+
