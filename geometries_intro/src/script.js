83b615c92a05fe5a47e24c80d776410a30bc7496
Omoyajowo Temidayo <temideewan@gmail.com>
2023-08-06 20:18:18 +0100

    chapter 9: learn about declaring custom geometries

--- geometries_intro/src/script.js
+++ geometries_intro/src/script.js
@@ -11,11 +11,19 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 // Object
-// const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
+// const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2); normal geometry
+const geometry = new THREE.BufferGeometry();
+const count = 5000;
+const positionsArray = new Float32Array(count * 3 * 3);
+for(let i = 0; i < count * 3 * 3; i++){
+  positionsArray[i] = (Math.random() - 0.5) * 4
+}
 
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
+geometry.setAttribute("position", positionsAttribute);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
 const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+scene.add(mesh);  
 
 // Sizes
 const sizes = {
@@ -37,6 +45,13 @@ window.addEventListener("resize", () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
+window.addEventListener("dblclick", () => {
+  if(!document.fullscreenElement){
+    canvas.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+})
 // Camera
 const camera = new THREE.PerspectiveCamera(
   75,
