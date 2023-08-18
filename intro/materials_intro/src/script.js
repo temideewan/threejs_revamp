dd9b88e198208fb5f69ab7e95559bc011311724f
Omoyajowo Temidayo <temideewan@gmail.com>
2023-08-18 07:44:01 +0100

    chore: continuing with learning about materials

--- intro/materials_intro/src/script.js
+++ intro/materials_intro/src/script.js
@@ -1,81 +1,134 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
-THREE.ColorManagement.enabled = false
+THREE.ColorManagement.enabled = false;
 
+// load textures
+const loadingManager = new THREE.LoadingManager(
+    () => console.log("loaded"),
+    () => console.log(" in progress"),
+    () => console.log("error loading textures")
+  );
+  const loader = new THREE.TextureLoader(loadingManager);
+  const alphaTexture = loader.load("/textures/door/alpha.jpg");
+  const ambientOcclusionTexture = loader.load("/textures/door/ambientOcclusion.jpg");
+  const colorTexture = loader.load("/textures/door/color.jpg");
+  const heightTexture = loader.load("/textures/door/height.jpg");
+  const metalnessTexture = loader.load("/textures/door/metalness.jpg");
+  const roughnessTexture = loader.load("/textures/door/roughness.jpg");
+  const normalTexture = loader.load("/textures/door/normal.jpg");
+  const firstGradientTexture = loader.load("/textures/gradients/3.jpg")
+  const matcapsTexture = loader.load("/textures/matcaps/3.png")
 /**
  * Base
  */
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
+/**
+ * Objects
+ */
+// const material = new THREE.MeshBasicMaterial();
+// material.map = colorTexture
+// material.color = new THREE.Color(0x00ff00)
+// material.wireframe = true
+// material.opacity = 0.4
+// material.transparent = true
+// material.alphaMap = alphaTexture
+// material.side = THREE.DoubleSide   
+// console.log(material.color);
+
+const material = new THREE.MeshNormalMaterial()
+
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), material);
+
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), material);
+
+const torus = new THREE.Mesh(
+  new THREE.TorusGeometry(0.5, 0.2, 16, 32),
+  material
+);
+plane.position.x = -1.5;
+torus.position.x = 1.5;
+scene.add(sphere, plane, torus);
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 2
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 2;
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.outputColorSpace = THREE.LinearSRGBColorSpace
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: canvas,
+});
+renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  // console.log(elapsedTime);
+  // update objects
+  sphere.rotation.y = 0.1 * elapsedTime;
+  plane.rotation.y = 0.1 * elapsedTime;
+  torus.rotation.y = 0.1 * elapsedTime;
+  sphere.rotation.x = 0.15 * elapsedTime;
+  plane.rotation.x = 0.15 * elapsedTime;
+  torus.rotation.x = 0.15 * elapsedTime;
 
-    // Update controls
-    controls.update()
+  // Update controls
+  controls.update();
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera);
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
 
-tick()
+tick();
