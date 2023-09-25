3213214c26d2888855a6fbcafdec99fbfe96378c
Omoyajowo Temidayo <temideewan@gmail.com>
2023-09-25 15:40:59 +0100

    feat: intro to the classic techniques

--- intro/text_intro/src/script.js
+++ intro/text_intro/src/script.js
@@ -57,7 +57,7 @@ fontLoader.load("fonts/helvetiker_regular.typeface.json", (font) => {
   const text = new THREE.Mesh(textGeometry, material);
   scene.add(text);
   const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
-  for(let i = 0; i < 100; i++){
+  for(let i = 0; i < 1000; i++){
     const donut = new THREE.Mesh(donutGeometry, material);
     donut.position.x = (Math.random() - 0.5) * 10;
     donut.position.y = (Math.random() - 0.5) * 10;
